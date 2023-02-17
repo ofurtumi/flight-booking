335c9500169bda12daff44888b6bd79ed134b55f
Gunnar <gbt6@hi.is>
2023-02-17 11:11:33 +0000

    First commit

---
+++ flugdb.sql
@@ -0,0 +1,10 @@
+/* Það þarf að uppfæra hvernig við ætlum að halda utanum prev flights og bookings*/
+CREATE TABLE User(
+    id VARCHAR(30),
+    name VARCHAR(30),
+    prev_flights VARCHAR(30),
+    prev_bookings VARCHAR(30)
+)
+CREATE TABLE Seat(
+    
+)
