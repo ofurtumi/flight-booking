ab5da1e0bc5cdca204c9ad02b872a83b5ed2fc20
hakoningir <hir12@hi.is>
2023-02-17 13:14:45 +0000

    Bookings, seat og SingleFlight í db

--- flugdb.sql
+++ flugdb.sql
@@ -6,5 +6,24 @@ CREATE TABLE User(
     prev_bookings VARCHAR(30)
 )
 CREATE TABLE Seat(
-    
+    id VARCHAR(3),
+    reserved BOOLEAN
+)
+/* Gæti þurft að laga date */
+CREATE TABLE SingleFlight(
+    fid VARCHAR(30) PRIMARY KEY,
+    seats VARCHAR(3) REFERENCES Seats(seats),
+    depAddress VARCHAR(50),
+    arriAddress VARCHAR(50),
+    depTime DATE,
+    arriTime DATE,
+    price INT
+)
+
+CREATE TABLE Bookings(
+    userid VARCHAR(30) REFERENCES User(id),
+    flightid VARCHAR(30) REFERENCES SingleFlight(fid),
+    seatnum VARCHAR(3) REFERENCES Seat(id),
+    bookingid VARCHAR(30),
+    passenger INT
 )
