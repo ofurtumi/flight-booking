8a26845a0f2b0504f6762370e753bd186db257d3
Gunnar <gbt6@hi.is>
2023-02-17 13:18:19 +0000

    User table

--- flugdb.sql
+++ flugdb.sql
@@ -1,10 +1,11 @@
-/* Það þarf að uppfæra hvernig við ætlum að halda utanum prev flights og bookings*/
+/*  
+    Það þarf að uppfæra hvernig við ætlum að halda utanum prev flights og bookings
+    UsierID er int fyrir Kt. spurning hvort að við viljum halda því eða gera random
+    string id.
+*/
 CREATE TABLE User(
-    id VARCHAR(30),
+    Userid INT,
     name VARCHAR(30),
     prev_flights VARCHAR(30),
     prev_bookings VARCHAR(30)
-)
-CREATE TABLE Seat(
-    
 )
