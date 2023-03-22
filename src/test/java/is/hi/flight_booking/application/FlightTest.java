088fa21473f36e059d0ddcc3f8ca1121458d543f
ofurtumi <thorvaldur.tumi@gmail.com>
2023-03-22 17:10:56 +0000

    Bæta við fleiri testum við nýja structurið

---
+++ src/test/java/is/hi/flight_booking/application/FlightTest.java
@@ -0,0 +1,52 @@
+package is.hi.flight_booking.application;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+public class FlightTest {
+  Flight flight;
+  Seat[] seats;
+
+  @Before
+  public void setUp() {
+    seats = new Seat[] {
+        new Seat("A-1", "F-304", false), new Seat("A-2", "F-304", false),
+        new Seat("A-3", "F-304", false), new Seat("B-1", "F-304", false),
+        new Seat("B-2", "F-304", false), new Seat("B-3", "F-304", false) };
+
+    flight = new Flight(
+        "F-304",
+        seats,
+        24000,
+        LocalDate.of(2023, 3, 21),
+        "Reykjavík",
+        LocalDate.now(),
+        "Keflavík");
+  }
+
+  @After
+  public void tearDown() {
+    seats = null;
+    flight = null;
+  }
+
+  @Test
+  public void testFlightGetArrivalAddress() {
+    assertEquals("Reykjavík", flight.getArrivalAddress());
+  }
+
+  @Test
+  public void testFlightGetDepartureAddress() {
+    assertFalse("Reykjavík".equals(flight.getDepartureAddress()));
+  }
+
+  @Test
+  public void testFlightPrice() {
+    assertEquals(24000, flight.getPrice());
+    flight.setPrice(30000);
+    assertEquals(30000, flight.getPrice());
+  }
+}
