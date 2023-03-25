03c43e06f684be6e2f8f843c66f181f073858883
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2023-03-25 18:22:48 +0000

    Export interfaces package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,5 @@ module is.hi.flight_booking {
     requires java.sql;
     exports is.hi.flight_booking.application;
     exports is.hi.flight_booking.controller;
+    exports is.hi.flight_booking.interfaces;
 }
