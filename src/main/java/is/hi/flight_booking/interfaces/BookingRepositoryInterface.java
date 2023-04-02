cbefafb0fa4778cf984e27250d4e78c61ce4c33c
ofurtumi <thorvaldur.tumi@gmail.com>
2023-04-02 23:34:39 +0000

    @hakoningir hérna er interface baby :<3:

---
+++ src/main/java/is/hi/flight_booking/interfaces/BookingRepositoryInterface.java
@@ -0,0 +1,66 @@
+package is.hi.flight_booking.interfaces;
+
+import is.hi.flight_booking.application.Booking;
+import is.hi.flight_booking.application.Flight;
+import is.hi.flight_booking.application.Seat;
+
+/**
+ * BookingRepositoryInterface
+ */
+public interface BookingRepositoryInterface {
+
+  /**
+   * Takes a {@link Booking} object and creates a version of it on the database
+   * 
+   * @param booking {@link Booking} object to be created on the database
+   */
+  public void createBooking(Booking booking);
+
+  /**
+   * Takes a {@link Booking} object and deletes it from the database
+   * needs to recursively remove seat reservations, can be implemented with
+   * {@link removeSeat}
+   * 
+   * @param booking {@link Booking} object to be deleted from the database
+   */
+  public void deleteBooking(Booking booking);
+
+  /**
+   * Ease of access method, implements both {@link deletebooking} and
+   * {@link createBooking}
+   * does not actually update booking rather removes it from the databse and
+   * creates it again with minor changes little different
+   *
+   * @param booking {@link Booking} object to be updated on the database
+   */
+  public void updateBooking(Booking booking);
+
+  /**
+   * Searches the database for a reference of the given {@link Seat} object and
+   * marks it as reserved as well as connecting it to the given {@link Booking} id
+   * 
+   * @param booking {@link Booking} object to be updated on the database
+   * @param seat    {@link Seat} object to be updated on the database
+   */
+  public void reserveSeat(Booking booking, Seat seat);
+
+  /**
+   * Searches the database for a reference to the given {@link Seat} object and
+   * marks it as not reserved as well as removing the connection between it and
+   * the given {@link Booking} it to the given {@link Booking} id
+   * 
+   * @param booking {@link Booking} object to be updated on the database
+   * @param seat    {@link Seat} object to be updated on the database
+   */
+  public void removeSeat(Booking booking, Seat seat);
+
+  /**
+   * Uses a combination of {@link removeSeat} and {@link reserveSeat} to update
+   * seats to fit the given {@link Bookings}
+   *
+   * @param booking {@link Booking} object to be updated on the database
+   * @param oldSeat {@link Seat} object to be updated on the database
+   * @param newSeat {@link Seat} object to be updated on the database
+   */
+  public void updateSeat(Booking booking, Seat oldSeat, Seat newSeat);
+}
--- src/main/java/is/hi/flight_booking/repository/BookingRepository.java
+++ src/main/java/is/hi/flight_booking/repository/BookingRepository.java
@@ -2,40 +2,78 @@ package is.hi.flight_booking.repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import is.hi.flight_booking.application.Booking;
+import is.hi.flight_booking.application.Seat;
 import is.hi.flight_booking.database.DB;
+import is.hi.flight_booking.interfaces.BookingRepositoryInterface;
 
-public class BookingRepository {
-    private final String connectionURL;
-    
-    public BookingRepository(String connectionURL) {
-        this.connectionURL = connectionURL;
-    }
+public class BookingRepository implements BookingRepositoryInterface {
+  private final String connectionURL;
 
-    public Booking getBooking(String bookingId) {
-        Booking found = null;
-        try {
-            DB db = new DB(connectionURL);
-            db.open();
-            ResultSet bookingRS = db.query("SELECT * FROM Bookings WHERE bookingId = ?",bookingId);
-            bookingRS.getString(bookingId);
-            // ResultSet userRS = db.query(
-            //     "SELECT * FROM User WHERE userId = SELELCT userId from Bookings where bookingId = ?", bookingId);
-            // ResultSet flightRS = db.query(
-            //     "SELECT * FROM Flights WHERE flightId = SELELCT flightId from Bookings where bookingId = ?",bookingId);
-            // ArrayList<Booking> bookings = new ArrayList<>();
-            // User userid = userRS.getString("userId");
-            // Flights flightid = flightRS.getString("flightId");
-            // found = new Booking(userid, flightid, bookingId);
-            db.close();
-        }
-        catch (SQLException e) {
-            System.err.println(e.getSQLState());
-            System.err.println(e.getMessage());
-            System.err.println(e.getErrorCode());
-        }
-        return found;
+  public BookingRepository(String connectionURL) {
+    this.connectionURL = connectionURL;
+  }
+
+  public Booking getBooking(String bookingId) {
+    Booking found = null;
+    try {
+      DB db = new DB(connectionURL);
+      db.open();
+      ResultSet bookingRS = db.query("SELECT * FROM Bookings WHERE bookingId = ?", bookingId);
+      bookingRS.getString(bookingId);
+      // ResultSet userRS = db.query(
+      // "SELECT * FROM User WHERE userId = SELELCT userId from Bookings where
+      // bookingId = ?", bookingId);
+      // ResultSet flightRS = db.query(
+      // "SELECT * FROM Flights WHERE flightId = SELELCT flightId from Bookings where
+      // bookingId = ?",bookingId);
+      // ArrayList<Booking> bookings = new ArrayList<>();
+      // User userid = userRS.getString("userId");
+      // Flights flightid = flightRS.getString("flightId");
+      // found = new Booking(userid, flightid, bookingId);
+      db.close();
+    } catch (SQLException e) {
+      System.err.println(e.getSQLState());
+      System.err.println(e.getMessage());
+      System.err.println(e.getErrorCode());
     }
+    return found;
+  }
+
+  @Override
+  public void createBooking(Booking booking) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createBooking'");
+  }
+
+  @Override
+  public void deleteBooking(Booking booking) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteBooking'");
+  }
+
+  @Override
+  public void updateBooking(Booking booking) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateBooking'");
+  }
+
+  @Override
+  public void reserveSeat(Booking booking, Seat seat) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'reserveSeat'");
+  }
+
+  @Override
+  public void removeSeat(Booking booking, Seat seat) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'removeSeat'");
+  }
+
+  @Override
+  public void updateSeat(Booking booking, Seat oldSeat, Seat newSeat) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateSeat'");
+  }
 }
