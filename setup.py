702602eb7f607eb69ff769f0481fa9d87aa1ca53
hakoningir <hir12@hi.is>
2023-04-02 15:04:46 +0000

    laga setup

--- setup.py
+++ setup.py
@@ -5,14 +5,10 @@ testpath = "test.db"
 
 # execute SQLite commands
 if os.name == 'nt':
-    path="db/flightBooking.db"
-    testpath = "db/test.db"
     cmd = f'cmd /c "sqlite3 {path} < db/schema.sql && sqlite3 ${path} < db/insert.sql"'
     test = f'cmd /c "sqlite3 {testpath} < db/schema.sql && sqlite3 {testpath} < db/insert.sql"'
 
 else:
-    path="flightBooking.db"
-    testpath = "test.db"
     cmd = f"cd db && sqlite3 {path} < schema.sql && sqlite3 {path} < insert.sql"
     test = f"cd db && sqlite3 {testpath} < schema.sql && sqlite3 {testpath} < insert.sql"
 
