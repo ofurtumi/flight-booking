7a86e215e4149241d5613f84f6560a9ecb0f26c4
ofurtumi <thorvaldur.tumi@gmail.com>
2023-04-07 14:05:56 +0000

    aðferðir skilgreindar í repo sem voru ekki útfærðar, núna útfærðar

--- src/main/java/is/hi/flight_booking/controller/FlightController.java
+++ src/main/java/is/hi/flight_booking/controller/FlightController.java
@@ -1,23 +1,41 @@
 package is.hi.flight_booking.controller;
 
 import is.hi.flight_booking.application.*;
-import is.hi.flight_booking.repository.FlightRepository;
 import java.time.LocalDate;
 import java.util.List;
 import is.hi.flight_booking.interfaces.FlightControllerInterface;
+import is.hi.flight_booking.repository.FlightRepository;
 
 public class FlightController implements FlightControllerInterface {
 
-  private List<Flight> flights;
-  // Á eftir að búa til FlightRepository
-  // private FlightRepository flightRepository;
+  private FlightRepository FR;
+
+  public FlightController(String URL) {
+    FR = new FlightRepository(URL);
+  }
 
   public Flight getFlight(String flightId) {
-    // Vantar
-    return null;
+    return FR.getFlight(flightId);
   }
 
   public List<Flight> searchFlights(String depAddress, String arrAddress, LocalDate depTime) {
-    return flights;
+    List<Flight> found = FR.searchFlights(depAddress, arrAddress, depTime);
+    return found;
+  }
+
+  public List<Flight> getSortedByPrice() {
+    return FR.getSortedByPrice();
+  }
+
+  public List<Flight> getSortedByTime() {
+    return FR.getSortedByTime();
+  }
+
+  public List<Flight> getSortedByDeparture() {
+    return FR.getSortedByDeparture();
+  }
+
+  public List<Flight> getSortedByArrival() {
+    return FR.getSortedByArrival();
   }
 }
