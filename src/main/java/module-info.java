def1c201c0d225c068f5cbb83d5430b952c1ffcb
ofurtumi <thorvaldur.tumi@gmail.com>
2023-04-11 13:44:36 +0000

    exporta ui og setja ui í opens

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,10 @@ module is.hi.flight_booking {
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
 
-  opens is.hi.flight_booking.application to javafx.fxml;
+  opens is.hi.flight_booking.ui to javafx.fxml;
 
   exports is.hi.flight_booking.application;
   exports is.hi.flight_booking.controller;
   exports is.hi.flight_booking.interfaces;
+  exports is.hi.flight_booking.ui;
 }
