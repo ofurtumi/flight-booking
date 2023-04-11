7870074aea50f0a66f36b501b3bee2a3269a0e61
ofurtumi <thorvaldur.tumi@gmail.com>
2023-04-11 11:23:49 +0000

    bætt við javafx í plugins

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,13 @@
 module is.hi.flight_booking {
-    requires java.sql;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
+  requires java.sql;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires com.fasterxml.jackson.core;
+  requires com.fasterxml.jackson.databind;
 
-    opens is.hi.flight_booking.application to javafx.fxml;
+  opens is.hi.flight_booking.application to javafx.fxml;
 
-    exports is.hi.flight_booking.application;
-    exports is.hi.flight_booking.controller;
-    exports is.hi.flight_booking.interfaces;
-  }
+  exports is.hi.flight_booking.application;
+  exports is.hi.flight_booking.controller;
+  exports is.hi.flight_booking.interfaces;
+}
