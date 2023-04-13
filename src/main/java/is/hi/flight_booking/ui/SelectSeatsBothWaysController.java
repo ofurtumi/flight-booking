8788534e65543d57d7119263f3cd8b187beaff1b
eirgud <eig28@hi.is>
2023-04-13 23:49:18 +0000

    Byrjaður að búta til controllerinn fyrir selectSeatsBothWays. Búinn að fx tagga hlutina sem gera eitthvað í fxmlinu

---
+++ src/main/java/is/hi/flight_booking/ui/SelectSeatsBothWaysController.java
@@ -0,0 +1,62 @@
+package is.hi.flight_booking.ui;
+
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SelectSeatsBothWaysController implements Initializable {
+
+    // ----------- FMXL parameters for Departure Flight --------------
+    // ---------------------------------------------------------------
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    // ------===== Departure Flight Seat Mouse Events ======-----------
+    // ----------------------------------------------------------------
+    public void fxDepA1Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxDepA2Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxDepB1Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxDepB2Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxDepC1Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxDepC2Click(MouseEvent mouseEvent) {
+    }
+    // ----------------------------------------------------------------
+
+    // -------====== Return Flight Seats Mouse Events =======----------
+    // ----------------------------------------------------------------
+
+    public void fxRetA1Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxRetA2Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxRetB1Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxRetB2Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxRetC1Click(MouseEvent mouseEvent) {
+    }
+
+    public void fxRetC2Click(MouseEvent mouseEvent) {
+    }
+    // ----------------------------------------------------------------
+}
