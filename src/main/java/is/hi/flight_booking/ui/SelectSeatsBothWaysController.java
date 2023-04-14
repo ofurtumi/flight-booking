34198da2d7f3952011e5f7bf7ae4f1727fe27286
eirgud <eig28@hi.is>
2023-04-14 00:07:01 +0000

    Búinn að setja upp fx breyturnar í select seats contr. Engin virkni

--- src/main/java/is/hi/flight_booking/ui/SelectSeatsBothWaysController.java
+++ src/main/java/is/hi/flight_booking/ui/SelectSeatsBothWaysController.java
@@ -1,17 +1,93 @@
 package is.hi.flight_booking.ui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class SelectSeatsBothWaysController implements Initializable {
 
-    // ----------- FMXL parameters for Departure Flight --------------
+    // ----------- FMXL breytur for Departure Flight -----------------
+    // ---------------------------------------------------------------
+    @FXML
+    private Text fxDepHeaderFlightNum;
+    @FXML
+    private Text fxDepFromLocation;
+    @FXML
+    private Text fxDepToLocation;
+    @FXML
+    private LocalDate fxDepDate;
+    @FXML
+    private Text fxDepSeatsSelected;
+    @FXML
+    private Text fxDepSeatsTotal;
+    @FXML
+    private VBox fxDepA1;
+    @FXML
+    private VBox fxDepA2;
+    @FXML
+    private VBox fxDepB1;
+    @FXML
+    private VBox fxDepB2;
+    @FXML
+    private VBox fxDepC1;
+    @FXML
+    private VBox fxDepC2;
+    @FXML
+    private TextField fxDepUserID;
+    @FXML
+    private Text fxDepMinorFlightNum;
+    @FXML
+    private Text fxDepTotalPrice;
     // ---------------------------------------------------------------
 
+    // ----------- FMXL breytur for Return Flight --------------------
+    // ---------------------------------------------------------------
+    @FXML
+    private Text fxRetHeaderFlightNum;
+    @FXML
+    private Text fxRetFromLocation;
+    @FXML
+    private Text fxRetToLocation;
+    @FXML
+    private LocalDate fxRetDate;
+    @FXML
+    private Text fxRetSeatsSelected;
+    @FXML
+    private Text fxRetSeatsTotal;
+    @FXML
+    private VBox fxRetA1;
+    @FXML
+    private VBox fxRetA2;
+    @FXML
+    private VBox fxRetB1;
+    @FXML
+    private VBox fxRetB2;
+    @FXML
+    private VBox fxRetC1;
+    @FXML
+    private VBox fxRetC2;
+    @FXML
+    private TextField fxRetUserID;
+    @FXML
+    private Text fxRetMinorFlightNum;
+    @FXML
+    private Text fxRetTotalPrice;
+    // ---------------------------------------------------------------
 
+    @FXML
+    private Text fxFinalPrice;
+
+    // ----------- Controller breytur --------------------------------
+    // ---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
